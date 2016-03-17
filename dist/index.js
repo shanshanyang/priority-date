67cffe7153898ad642060c210f1fbbd6bf5674bd
Shanshan Yang <office.ss.nyc@gmail.com>
2016-03-17 16:36:31 +0000

    clean up

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-				value: true
+  value: true
 });
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
@@ -11,61 +11,52 @@ var request = require('request');
 var Promise = require('bluebird');
 var jsdom = require('jsdom');
 
-var currentMonth = new Date().getUTCMonth() + 1,
-    nextMonth = currentMonth + 1;
-
-if (nextMonth > 12) nextMonth = 1;
-
-var year = new Date().getFullYear();
-var next = {},
-    current = {};
-
 var createID = function createID(month, year) {
-				return month + '-' + year;
+  return month + '-' + year;
 };
-// request scrapper page
-var parseData = function parseData(body) {
-				var _ref;
 
-				jsdom.defaultDocumentFeatures = {
-								FetchExternalResources: ['script'],
-								ProcessExternalResources: ['script'],
-								MutationEvents: '2.0',
-								QuerySelector: false
-				};
-				var $c = cheerio.load(body);
-				var script = $c('script[charset=utf-8]')[0].children[0].data;
-				// need to stip out the closure code for jsdom defaultView to work
-				var regex = /([^\*\/]*)(?=\/\*)/g;
-				script = script.match(regex)[1];
+var parseScript = function parseScript(body) {
+  return cheerio.load(body)('script[charset=utf-8]')[0].children[0].data.match(/([^\*\/]*)(?=\/\*)/g)[2];
+};
 
-				var window = jsdom.jsdom('<script>' + script + '</script>').defaultView;
+var getData = function getData(body) {
+  var _ref;
 
-				var nextData = window.upComingMonths;
-				var currentData = window.thisMonths;
+  jsdom.defaultDocumentFeatures = {
+    FetchExternalResources: ['script'],
+    ProcessExternalResources: ['script'],
+    MutationEvents: '2.0',
+    QuerySelector: false
+  };
 
-				next = Object.assign(next, nextData);
-				current = Object.assign(current, currentData);
-				var nextID = createID(nextMonth, year);
-				var currentID = createID(currentMonth, year);
-				return _ref = {}, _defineProperty(_ref, nextID, next), _defineProperty(_ref, currentID, current), _ref;
+  var currentMonth = new Date().getUTCMonth() + 1,
+      nextMonth = currentMonth + 1;
+
+  if (nextMonth > 12) nextMonth = 1;
+
+  var year = new Date().getFullYear(),
+      window = jsdom.jsdom('<script>' + parseScript(body) + '</script>').defaultView,
+      nextID = createID(nextMonth, year),
+      currentID = createID(currentMonth, year);
+
+  return _ref = {}, _defineProperty(_ref, nextID, window.upComingMonths), _defineProperty(_ref, currentID, window.thisMonths), _ref;
 };
 
 var priorityDate = function priorityDate(params) {
-				// check params type
-				var options = Object.assign({
-								url: 'http://travel.state.gov/content/visas/en/immigrate/immigrant-process/approved/checkdate.html'
-				}, params);
+  var options = Object.assign({
+    url: 'http://travel.state.gov/content/visas/en/immigrate/immigrant-process/approved/checkdate.html'
+  }, params),
+      defer = Promise.defer();
 
-				var defer = Promise.defer();
-				request.get(options.url, function (error, response, body) {
-								if (error || response.statusCode != 200) {
-												defer.reject(error);
-								} else {
-												defer.resolve(parseData(body));
-								}
-				});
-				return defer.promise;
+  request.get(options.url, function (error, response, body) {
+    if (error || response.statusCode != 200) {
+      defer.reject(error);
+    } else {
+      defer.resolve(getData(body));
+    }
+  });
+
+  return defer.promise;
 };
 
 exports.default = priorityDate;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ const createID = (month, year) => {
 	return `${month}-${year}`;
 };
 
-const parseData = body => {
+const parseScript = body => {
+  return cheerio.load(body)('script[charset=utf-8]')[0]
+          .children[0]
+          .data
+          .match(/([^\*\/]*)(?=\/\*)/g)[2];
+};
+
+const getData = body => {
   
 	jsdom.defaultDocumentFeatures = {
 	  FetchExternalResources: ['script'],
@@ -22,13 +29,10 @@ const parseData = body => {
   if (nextMonth > 12) nextMonth = 1;
 
 	const year = new Date().getFullYear(),
-	      script = cheerio.load(body)('script[charset=utf-8]')[0]
-	                .children[0]
-	                .data.match(/([^\*\/]*)(?=\/\*)/g)[1],
-	      window = jsdom.jsdom(`<script>${script}</script>`).defaultView,
+	      window = jsdom.jsdom(`<script>${parseScript(body)}</script>`).defaultView,
 	      nextID = createID(nextMonth, year),
 	      currentID = createID(currentMonth, year);
-	
+
 	return {
 	  [nextID]: window.upComingMonths, 
 	  [currentID]: window.thisMonths
@@ -47,7 +51,7 @@ const priorityDate = params => {
         if(error || response.statusCode != 200) {
           defer.reject(error);
         } else {
-          defer.resolve(parseData(body));
+          defer.resolve(getData(body));
         }
     });
     
